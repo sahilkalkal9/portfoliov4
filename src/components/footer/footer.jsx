e47aee8b7d441767d4f775c6e5bf44ce7db6db2d
Sahil Kalkal <sahilkalkal108@gmail.com>
2024-11-12 16:27:47 +0530

    nav changes

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,22 @@ function Footer() {
     return (
         <div className="footer">
             <div className="footer-box">
-                <img src={require("../nav/logo.png")} alt="" className="fimg" />
+                <img src={require("../nav/logo.png")} alt="Logo" className="fimg" />
+                <div className="footer-socials">
+                    <div className="fsocial">
+                        <img src={require("./linkedin.png")} alt="LinkedIn" className="fsimg" />
+                        <p className="fstext">
+                            LinkedIn
+                        </p>
+                    </div>
+                    <div className="fsocial">
+                        <img src={require("./github.png")} alt="LinkedIn" className="fsimg" />
+                        <p className="fstext">
+                            GitHub
+                        </p>
+                    </div>
+
+                </div>
             </div>
         </div>
     )
